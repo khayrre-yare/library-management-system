c21b93fd4fbe22d031c26f5327eebf8a8993535c
Haydar Ali Omar <owhaydar@gmail.com>
2026-07-21 20:39:33 +0400

    Require manual backend startup

--- install-autostart.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0install-autostart.ps1"
-pause
--- uninstall-autostart.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Unregister-ScheduledTask -TaskName 'Jamhuuriyo Library Auto Start' -Confirm:$false -ErrorAction SilentlyContinue"
-echo Jamhuuriyo Library auto-start has been removed.
-pause
